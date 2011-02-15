6ccdc426f9bc454516700d7dcff4995146013674
Michael Huang <mike.g.huang@gmail.com>
2011-02-14 22:08:52 -0500

    Add support for Logitech G27 and perspective toggle.

--- src/org/pihisamurai/robot/Robot.java
+++ src/org/pihisamurai/robot/Robot.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
 
+    final boolean G27 = true; /* G27 = steering wheel controls */
+
     final int PWM_SLOT = 4; /* Where the PWM module is plugged */
 
     final int PWM_RIGHT_1 = 4; /* Front right. (River) */
@@ -32,6 +34,8 @@ public class Robot extends IterativeRobot {
     Drivetrain drivetrain;
     Joystick joystick1;
 
+    ToggleListener perspectiveToggle;
+
     public void robotInit() {
         jr1 = new JaguarHelper(PWM_SLOT, PWM_RIGHT_1);
         jr2 = new JaguarHelper(PWM_SLOT, PWM_RIGHT_2);
@@ -40,6 +44,13 @@ public class Robot extends IterativeRobot {
 
         drivetrain = new Drivetrain(jr1, jr2, jl1, jl2);
         joystick1 = new Joystick(1);
+
+        if(G27) {
+            perspectiveToggle = new ToggleListener(8);
+        }
+        else {
+
+        }
     }
 
     public void autonomousPeriodic() {
@@ -48,20 +59,56 @@ public class Robot extends IterativeRobot {
 
     public void teleopPeriodic() {
 
-        if(!joystick1.getRawButton(1)) { /* Trigger is brake */
+        if(G27) {
+
+            int reverse = 1;
+
+            /* Perspective reversal */
+            if(perspectiveToggle.on) {
+                reverse = -1;
+            }
+
+            if(!joystick1.getRawButton(5) && !joystick1.getRawButton(6)) {
+            /* Positive throttle is brake, negative throttle is accelerate */
+            /* Pedals must be on single axis mode */
+            drivetrain.setBrakeRight(false);
+            drivetrain.setBrakeLeft(false);
+            PWM_CURRENTSPEED = 1;
+            drivetrain.setRightSpeed(joystick1.getX()+joystick1.getThrottle()
+                    *reverse);
+            drivetrain.setLeftSpeed(-joystick1.getX()+joystick1.getThrottle()
+                    *reverse);
+            }
+            else {
+            drivetrain.setRightSpeed(0);
+            drivetrain.setLeftSpeed(0);
+            drivetrain.setBrakeRight(true);
+            drivetrain.setBrakeLeft(true);
+            }
+        }
+        else {
+            if(!joystick1.getRawButton(1)) { /* Trigger is brake */
             drivetrain.setBrakeRight(false);
             drivetrain.setBrakeLeft(false);
                 PWM_CURRENTSPEED = PWM_NOMINALSPEED*((1-joystick1.getZ())/2);
             drivetrain.setRightSpeed(joystick1.getY()+joystick1.getX());
             drivetrain.setLeftSpeed(joystick1.getY()-joystick1.getX());
             }
-        /* Braking */
-        else {
+            /* Braking */
+            else {
             drivetrain.setRightSpeed(0);
             drivetrain.setLeftSpeed(0);
             drivetrain.setBrakeRight(true);
             drivetrain.setBrakeLeft(true);
+            }
         }
+
+        printButtonsHeld();
+
+        /*System.out.println("Joystick X: "+joystick1.getX());
+          System.out.println("Joystick Y: "+joystick1.getY());
+          System.out.println("Joystick Z: "+joystick1.getZ());
+          System.out.println("Joystick T: "+joystick1.getThrottle());*/
     }
 
     /* Simple drivetrain class for four motor tank drive */
@@ -140,5 +187,66 @@ public class Robot extends IterativeRobot {
         }
     }
 
- }
+    /* Usage: ToggleListener tl = new ToggleListener(buttonNumber). Read boolean
+       from tl.on for toggle value.  */
+    private class ToggleListener implements Runnable {
 
+        int buttonNumber;
+        int pollRate = 10;
+        Thread thread;
+        boolean run = true;
+        boolean isPressed = false;
+        boolean on = false;
+
+
+    	ToggleListener(int buttonNumber) {
+            this.buttonNumber = buttonNumber;
+            thread = new Thread(this);
+            thread.start();
+    	}
+
+        ToggleListener(int buttonNumber, int pollRate) {
+            this.pollRate = pollRate;
+            this.buttonNumber = buttonNumber;
+            thread = new Thread(this);
+            thread.start();
+    	}
+
+    	public void run() {
+            while(run) {
+                boolean wasPressed = isPressed;
+
+                try {
+                if(joystick1.getRawButton(buttonNumber)) {
+                    isPressed = true;
+                }
+                else {
+                    isPressed = false;
+                }
+
+                if(wasPressed && !isPressed) {
+                    on = !on;
+                }
+
+                Thread.sleep(pollRate);
+                }
+                catch(Exception e) {
+                    System.out.println("Exception: "+e);
+                }
+            }
+    	
+        }
+
+        public void stop() {
+            run = false;
+        }
+        }
+
+    private void printButtonsHeld() {
+        for(int i = 1; i <= 30; i++) {
+            boolean held = joystick1.getRawButton(i);
+            if(held)
+            System.out.println("Button "+i+": "+held);
+        }
+    }
+ }
